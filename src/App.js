85294df57552402220ee52a953766e749cc0ef6a
Rahul255 <rahulmohan255@gmail.com>
2020-05-04 19:12:01 +0530

    removed styled components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-
 class App extends Component  {
     state = {
         persons: [
@@ -48,11 +47,16 @@ class App extends Component  {
 
     render(){
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1x solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
 
         let  persons = null;
@@ -70,18 +74,31 @@ class App extends Component  {
 
                 </div>
             );
+            // style.backgroundColor = 'red';
+            // style[':hover'] = {
+            //     backgroundColor: 'salmon',
+            //         color: 'black'
+            // };
         }
 
+        const classes = [];
+        if (this.state.persons.length <= 2) {
+            classes.push('red'); // classes = [red]
+        }
+        if (this.state.persons.length <= 1) {
+            classes.push('bold'); // classes = [red,bold]
+        }
 
         return (
-            <div className="App">
-                <h1>Hi this is a react app</h1>
-                <p>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>Toggle Persons</button>
-                {persons}
-            </div>
+
+                <div className="App">
+                    <h1>Hi this is a react app</h1>
+                    <p className={classes}>This is really working!bold</p>
+                    <button className="button"
+                        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                    {persons}
+                </div>
+
         );
     }
 
